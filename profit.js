0699e7de37ab09288dbeb7f3b2bd667e252fa1c6
Samaritain Sims <sfaysamaritan@gmail.com>
2021-12-05 00:28:12 +0100

    mdpairtimes lighter

--- profit.js
+++ profit.js
@@ -6,7 +6,7 @@ const {Decimal} = require('decimal.js');
 
 const DateUtils = require('./utils/dateutils');
 
-const MAX_PART_NB_TX = 100;
+const MAX_PART_NB_TX = 500;
 
 const convertInitialAmount = function (strategyVars, amount, coin, bookTicker, tripletsData) {
     let stables = strategyVars["stableCoins"];
@@ -518,15 +518,15 @@ const calculateProfit = function (strategyVars, tripletsData, bookTicker, update
 
             let mdPairsTimes = [];
             if (trades.length > 0) {
-                Object.keys(tripletsData["marketData"]["mdPairsData"])
-                    .forEach((mdp) => {
-                        let mdpTime = trades.filter((t) => {
-                            return ((tripletsData["mdpToTriplets"][mdp] || []).includes(t["tripletId"]))
-                        }).map((t) => {
-                            return t["time"];
-                        }).reduce((a, b) => a+b, 0);
-                        mdPairsTimes.push({mdp: mdp, time: mdpTime});
+                let temp = {};
+                trades.forEach((trade)=>{
+                    tripletsData["tripletsToMdp"][trade["tripletId"]].forEach((mdp)=>{
+                        temp[mdp] = temp[mdp] || [];
+                        temp[mdp].push(trade["time"])
                     });
+                });
+                mdPairsTimes = Object.keys(temp)
+                    .map(mdp => {return {mdp: mdp, time: temp[mdp].reduce((a,b)=>a+b,0)}});
                 mdPairsTimes.sort((a, b) => {
                     if (a.time > b.time) return -1;
                     else if (a.time < b.time) return 1;
